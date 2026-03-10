321bc65df5471d010ff042868cd6e6e2e180520e
Toby Dragon <dr.toby.dragon@gmail.com>
2026-03-10 19:21:01 -0400

    fixed attendee color change

--- attendee.gd
+++ attendee.gd
@@ -1,10 +1,12 @@
-extends Sprite2D
+extends Node2D
 
-@export var happiness:int:
+@onready var color_node = $Color
+
+@export var happiness:int = 0:
 	set(new_value):
 		assert (new_value >=-100 and new_value <= 100)
 		happiness = new_value
 		if happiness > 0:
-			self.modulate = Color(0, happiness/100.0, 0)
+			color_node.modulate = Color(0, 1, 0, happiness/100.0)
 		elif happiness < 0:
-			self.modulate = Color(-happiness/100.0, 0, 0)
+			color_node.modulate = Color(1, 0, 0, -happiness/100.0)
