e8c208900597314078c4bc6bd35183fd1225c43a
Toby Dragon <tdragon@ithaca.edu>
2026-03-12 06:51:35 -0400

    added a test of sprite color

--- test/test_attendee.gd
+++ test/test_attendee.gd
@@ -5,6 +5,10 @@ func test_set_happiness():
 	add_child_autofree(attendee) # gut should add and remove from scene tree
 	attendee.happiness = 50
 	assert_eq(attendee.happiness, 50)
+	# check that the color setting worked
+	assert_almost_eq(attendee.color_node.modulate.g, 1, 0.001)
+	assert_almost_eq(attendee.color_node.modulate.a, 0.5, 0.001)
+	
 	attendee.happiness = -50
 	assert_eq(attendee.happiness, -50)
 	attendee.happiness = 100
@@ -12,5 +16,8 @@ func test_set_happiness():
 	attendee.happiness = -100
 	assert_eq(attendee.happiness, -100)
 	
+	attendee.happiness = 3
+	assert_eq(attendee.happiness, 3)
 	attendee.happiness = 101
 	assert_push_error("invalid value for happiness")
+	assert_eq(attendee.happiness, 3) 
