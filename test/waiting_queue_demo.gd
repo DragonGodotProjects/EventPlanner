9402d2abc7874b4239d34c61e382de9b80a56be2
Toby Dragon <tdragon@ithaca.edu>
2026-03-15 15:58:51 -0400

    updated demo to work with empty queue dequeue

--- test/waiting_queue_demo.gd
+++ test/waiting_queue_demo.gd
@@ -4,12 +4,15 @@ var rng = RandomNumberGenerator.new()
 @onready var attendee_scene:PackedScene = preload("../attendee.tscn")
 @onready var waiting_queue:WaitingQueue = $WaitingQueue
 
+func _ready():
+	waiting_queue.dequeued.connect(on_finish_dequeue)
+
+
 func _input(event:InputEvent):
 	if event.is_action_pressed("ui_up"):
 		var new_attendee:Attendee = attendee_scene.instantiate()
 		waiting_queue.enqueue(new_attendee)
 	elif event.is_action_pressed("ui_down"):
-		waiting_queue.dequeued.connect(on_finish_dequeue)
 		waiting_queue.start_dequeue()
 		
 func on_finish_dequeue(attendee:Attendee):
