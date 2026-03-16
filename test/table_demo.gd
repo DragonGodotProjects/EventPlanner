408ebbf627e202a9fe0660e25880765005522a70
Toby Dragon <dr.toby.dragon@gmail.com>
2026-03-16 01:06:56 -0400

    updated table demo

--- test/table_demo.gd
+++ test/table_demo.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+var rng = RandomNumberGenerator.new()
+
 @onready var attendee_scene:PackedScene = preload("../attendee.tscn")
-@onready var table1:Table = $Table
+@onready var table:Table = $Table
 
 func _input(event:InputEvent):
 	if event.is_action_pressed("ui_up"):
 		var new_attendee:Attendee = attendee_scene.instantiate()
-		print(table1.seat_attendee(new_attendee))
+		new_attendee.position = Vector2i(rng.randi_range(50, 400), rng.randi_range(50, 400))
+		var seat_num = table.find_open_seat()
+		if seat_num != -1:
+			table.seat_attendee(new_attendee, seat_num)
+		else:
+			print("No seat available")
